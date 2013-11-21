6d43e84c3e3c6de1b3b87ff3a19ae8f23d7bd0c2
zalaj <zala.jamsek@gmail.com>
2013-11-21 18:31:48 +0100

    funkcija za računanje posterior verjetnosti
    Dodana je funkcija Naive Bayes, ki izračuna posteriorni verjetnosti in
    jih primerja.
    Popravljena imena spremenljivk

--- Bayes_naive_classifier.py
+++ Bayes_naive_classifier.py
@@ -1,7 +1,7 @@
 ##moj X je class, za katerega iščemo klasifikator
 
 ##-------
-##Funkcija za branje CSV, ki vrne slovar z vrednostmi
+##Funkcija za branje CSV, ki vrne slovar z vredNostmi
 ##-------
 def read_csv (csv_file):
     
@@ -11,7 +11,7 @@ def read_csv (csv_file):
 
     rownum=0
     header=[]
-    values={}
+    training_data={}
     
     for row in read:
         row=row[0].rsplit(';')
@@ -21,87 +21,89 @@ def read_csv (csv_file):
             colnum=0
 
             for colnum in range (0, len(header)):
-                values[header[colnum]]=[]
+                training_data[header[colnum]]=[]
                 colnum=colnum+1
             
         else:
             colnum=0
 
             for colnum in range (0, len(header)):
-                values[header[colnum]].append(row[colnum])
+                training_data[header[colnum]].append(row[colnum])
                 colnum=colnum+1
                 
         rownum=rownum+1
-    return(values)
+    return(training_data)
 
-
+training_data = read_csv('vaja_podatki.csv') 
 ##-------
-##Funkcija, ki iz slovarja s CSV vrne zalogo unique vrednosti za vsak atribut
+##Funkcija, ki iz slovarja s CSV vrne zalogo unique vredNosti za vsak atribut
 ##-------
 
-def unique_values(values):
+def unique_values(training_data):
     
-    keys = list(values.keys())
+    keys = list(training_data.keys())
     unique_values={}
     colnum=0
     
     for col in keys:
-        ##za vsak ključ naredimo množico vrednosti in jo spremenimo nazaj v seznam
-        ##tako dobimo unique vrednosti za vsak atribut
-        unique_values[col]= list(set(values[col]))
+        ##za vsak ključ naredimo mNožico vredNosti in jo spremenimo nazaj v seznam
+        ##tako dobimo unique vredNosti za vsak atribut
+        unique_values[col]= list(set(training_data[col]))
     return(unique_values)
 
 
 ##-------
-##Funkcija, ki izrčuna pogojne verjetnosti za atribute z diskretnimi vrednostmi
+##Funkcija, ki izrčuna pogojne verjetNosti za atribute z diskretnimi vredNostmi
 ##-------
 
-##Tu privzamem, da so pogojne vrednosti lahko samo Yes ali No
+##Tu privzamem, da so pogojne vredNosti lahko samo Yes ali No
 
-def discrete(values, atribute, identificator):
+def discrete(training_data, atribute, class_variable, test_record):
     
-    ##values - slovar, kjer so ključi atributi, vrednosti pa so seznam vrednosti
-    ##atribute - atrinut za katerega išečmo pogojne verjetnosti
-    ##identificator je tisti atribut glede na katerega gledaš pogojno verjetnost
-    ##- DEFAULT BORROWER
+    ##training_data - slovar, kjer so ključi atributi, vredNosti pa so seznam vredNosti
+    ##atribute - atrinut za katerega išečmo pogojne verjetNosti
+    ##class_variable = atribut glede na katerega iščemo pogojNo verjetNost (DEFAULT BORROWER)
 
-    ##funkcija z uniwue vrednostmi za atrinute in zaloga vrednosti 
-    unique=unique_values(values)  
+    ##pokličemo funkcijo z unique vredNostmi za atrinute in zaloga vredNosti 
+    unique=unique_values(training_data)  
     z_vr = unique.get(atribute)
 
     #podatki za iskani atribut 
-    data_a = values.get(atribute)
+    data_a = training_data.get(atribute)
 
     #podatki za identifikator
-    data_i = values.get(identificator)
+    data_i = training_data.get(class_variable)
     
-    p_yes={}
-    p_no={}    
-    frek_no=0
-    frek_yes=0
+    p={}
+    p['Yes']=0
+    p['No']=0
     
-    for col in z_vr:
-        p_yes[col]=0
-        p_no[col]=0
- 
+    frek_No=0
+    frek_Yes=0
+    
     for col in range (0, len(data_a)):
         
         if data_i[col]=='Yes':
-            p_yes[data_a[col]] = p_yes[data_a[col]]+1
-            frek_yes=frek_yes+1
+            frek_Yes=frek_Yes+1
+            if data_a[col]==test_record:
+                p['Yes']=p['Yes']+1
             
-        if data_i[col]=='No':
-            p_no[data_a[col]] = p_no[data_a[col]]+1
-            frek_no=frek_no+1
             
-    for col in z_vr:
-        p_yes[col]=p_yes[col]/frek_yes
-        p_no[col]=p_no[col]/frek_no
+        if data_i[col]=='No': 
+            frek_No=frek_No+1
+            if data_a[col]==test_record:
+                p['No'] = p['No']+1
+            
+    p['Yes'] = p['Yes']/frek_Yes
+    p['No']=p['No']/frek_No
     
-    return(p_yes, p_no)
+    return(p)
+
+d = discrete(training_data,'Home owner', 'Default borrower', 'No')
+e = discrete(training_data,'Maritual status', 'Default borrower', 'Married')
 
 ##-------
-##Funkcija, ki izrčuna upanje vrednosti v nizu
+##Funkcija, ki izrčuna upanje vredNosti v nizu
 ##----
 def mean(list):
     
@@ -110,7 +112,7 @@ def mean(list):
 
 
 ##-------
-##Funkcija, ki izrčuna standardni odklon vrednosti v nizu
+##Funkcija, ki izrčuna standardni odklon vredNosti v nizu
 ##----
 def sd(list):
     
@@ -123,10 +125,10 @@ def sd(list):
     return(sd)
 
 ##-------
-##Funkcija, ki izrčuna verjetnost za normalno porazdeljene podatke
+##Funkcija, ki izrčuna verjetNost za NormalNo porazdeljene podatke
 ##----
-def p_normal (ident, mean, sd):
-    ##ident je identifikator za katerega računamo verjetnost
+def p_Normal (ident, mean, sd):
+    ##ident je identifikator za katerega računamo verjetNost
 
     import math
     
@@ -135,59 +137,126 @@ def p_normal (ident, mean, sd):
     
 
 ##-------
-##Funkcija, ki izrčuna pogojne verjetnosti za atribute z normalno porazdelitvijo
+##Funkcija, ki izrčuna pogojne verjetNosti za atribute z NormalNo porazdelitvijo
 ##----
 
-def normal(values, atribute, identificator, clas):
+def Normal(training_data, atribute, class_variable, test_record):
     
-    ##values - slovar, kjer so ključi atributi, vrednosti pa so seznam vrednosti
-    ##atribute - atrinut za katerega išečmo pogojne verjetnosti
-    ##identificator je tisti atribut glede na katerega gledaš pogojno verjetnost
+    ##values - slovar, kjer so ključi atributi, vredNosti pa so seznam vredNosti
+    ##atribute - atrinut za katerega išečmo pogojne verjetNosti
+    ##identificator je tisti atribut glede na katerega gledaš pogojNo verjetNost
     ##- DEFAULT BORROWER
     ##clas - tisti razred za katerega delamo posterior 
 
-    ##funkcija z uniwue vrednostmi za atrinute in zaloga vrednosti 
-    unique=unique_values(values)  
+    ##funkcija z uniwue vredNostmi za atrinute in zaloga vredNosti 
+    unique=unique_values(training_data)  
     z_vr = unique.get(atribute)
 
     #podatki za iskani atribut 
-    data_a = values.get(atribute)
+    data_a = training_data.get(atribute)
 
     #podatki za identifikator
-    data_i = values.get(identificator)
-    z_vr_i = unique.get(identificator)
+    data_i = training_data.get(class_variable)
+    z_vr_i = unique.get(class_variable)
     
-    p={}
-    frek_yes=0
-    frek_no=0
+    p_data={}
+    frek_Yes=0
+    frek_No=0
 
     for i in z_vr_i:
-        p[i]=[]
+        p_data[i]=[]
 
     for col in range(0, len(data_a)):
         
         if data_i[col]=='Yes':
-            p['Yes'].append(int(data_a[col]))
-            frek_yes = frek_yes + 1
+            p_data['Yes'].append(int(data_a[col]))
+            frek_Yes = frek_Yes + 1
         if data_i[col]=='No':
-            p['No'].append(int(data_a[col]))
-            frek_no = frek_no + 1
+            p_data['No'].append(int(data_a[col]))
+            frek_No = frek_No + 1
 
+    p={}
+    
     for i in z_vr_i:
-        m = int(mean(p[i]))
-        s = int(sd(p[i]))
-        p = p_normal(clas,m,s)
-        return(p)
+        m = int(mean(p_data[i]))
+        s = int(sd(p_data[i]))
+        p[i]=(p_Normal(test_record,m,s))
+
+    return(p)
+           
+###----
+##Funkcija za prior
+###---
+def prior(training_data, class_variable):
+
+    c_variable= training_data[class_variable]
+    
+    p={}
+    p['No']= c_variable.count('No')/len(c_variable)
+    p['Yes'] = c_variable.count('Yes')/len(c_variable)
+    
+    return(p)
+
+###----
+##Produkt
+###
+
+def prod(list):
+
+    p=1
+    for i in list:
+       p*=i
+    return(p)
+    
+    
+#####---
+##Funkcija za bayes
+####---
+
+def bayes_naive_class (csv_file, test_record, class_variable):
+
+    ##csv_file = csv datoteka, ki vsebuje podatke
+    ##test_record = slovar podatkov za katere iščemo napoved
+    ##class_variable = class glede na katerega gledamo pogojne verjetNosti
 
 
-def bayes_naive_class (csv_file, Class, identificator):
+    training_data = read_csv(csv_file)    
+    unique = unique_values(training_data)
 
-    data = read_csv(csv_file)    
-    print(data)
+    prior_distr = prior(training_data, class_variable)
+    
+    p_Yes=[]
+    p_No=[]
 
-s = {'Home owner':['No'],'Maritual status': ['Single'], 'Annual income': [120]}
-X='Default borrower'
-bayes_naive_class ('vaja_podatki.csv',s,X)
+    for atribute in test_record:
+        
+        if len(unique[atribute])<=5:
+            x_i = test_record[atribute]
+            p = discrete (training_data, atribute, class_variable, x_i)
+            p_Yes.append(p['Yes'])
+            p_No.append(p['No'])
+
+        else:
+            x_i = int(test_record[atribute])
+            p = Normal(training_data, atribute, class_variable, x_i)
+            p_Yes.append(p['Yes'])
+            p_No.append(p['No'])
+
+    ##posteriorna verjetnost je produkt vseh pogojnih verjetnosti in priorne verjetnosti
+    post_yes = prod(p_Yes)*prior_distr['Yes']
+    post_no = prod(p_No)*prior_distr['No']
+
+    if post_yes > post_no:
+        return('Yes')
+    else:
+        return('No')
+    
+
+#########################################   
+test_record = {'Home owner':'No','Maritual status': 'Married', 'Annual income': 120}
+class_variable='Default borrower'
+
+vaja = bayes_naive_class ('vaja_podatki.csv', test_record, class_variable)
 
     
     
